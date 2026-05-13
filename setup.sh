486b45ab69278a8f52b1df49c9cfc67f56042b4f
Alan Correa <correa@mbd.rwth-aachen.de>
2026-05-13 12:44:31 +0200

    update setup.sh script

--- setup.sh
+++ setup.sh
@@ -4,11 +4,9 @@ set -e
 
 echo "=== Starting Topographic UQ Project Setup ==="
 
-echo -e "\n[1/2] Building Orchestrator and Analysis Environments..."
+echo -e "\n Building Environments..."
+micromamba env create -f env-model.yml
 micromamba env create -f env-montecarlo.yml
 micromamba env create -f env-energy-analysis.yml
-
-echo -e "\n[2/2] Building Physics Environment (env-model)..."
-micromamba env create -f env-model.yml
 
 echo -e "\n=== Setup Complete! ==="
